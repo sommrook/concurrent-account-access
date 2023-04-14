e9d45824185b59c75081137907dbf0c2bd431291
sommrook-42maru <sommrook@gmail.com>
2023-04-14 09:00:05 +0900

    :art: structure refactor

--- asgi.py
+++ asgi.py
@@ -1,9 +1,9 @@
 import uvicorn
-from src.metadata import API_HOST, API_PORT, API_WORKERS
+from app.core.settings import API_HOST, API_PORT, API_WORKERS
 
 if __name__ == "__main__":
     uvicorn.run(
-        "src.server:app",
+        "app.server:app",
         host=API_HOST,
         port=API_PORT,
         workers=API_WORKERS
