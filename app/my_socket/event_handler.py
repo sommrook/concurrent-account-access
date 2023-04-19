cd841939d91f8e9f97ea7ea1facebe61d9464590
sommrook-42maru <sommrook@gmail.com>
2023-04-19 13:40:34 +0900

    :truck: fastapi_consumer.py -> socket_consumer.py

--- app/my_socket/event_handler.py
+++ app/my_socket/event_handler.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from app.my_socket.fastapi_consumer import socket_consumer, send_consumer_message
+from app.my_socket.socket_consumer import socket_consumer, send_consumer_message
 from app.core.log_manager import access_logger
 
 async def consume():
